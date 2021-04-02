60dc621b359ad7735d85d95fe4c33cb1eeb5a497
Ryan <ryan.moussouni@sagemcom.com>
2021-04-02 10:50:34 +0200

    changed model

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, Activation
+from tensorflow.keras.optimizers import Adam
+
+
 class SRCNN(tf.keras.Model):
 	def __init__(self):
-		self.input = Input(input_shape=(2160, 3840, 1))
+		self.input = Input(shape=(2160, 3840, 1))
 		self.conv1 = Conv2D(64,9,padding='same')
 		self.act1 = Activation('relu')
 		self.conv2 = Conv2D(32,1,padding='same')
